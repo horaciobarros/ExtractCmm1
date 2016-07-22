6f19b0e72a9ec32909ec349229cdd6ed9e559730
FernandoWerneck <nandowerneck@hotmail.com.br>
2016-07-22 18:07:02 -0300

    Valida Cnpj

--- src/cmm/util/Util.java
+++ src/cmm/util/Util.java
@@ -17,10 +17,8 @@ import cmm.entidadesOrigem.DadosLivroPrestador;
 import cmm.model.Tomadores;
 
 public class Util {
-	
-	private static final String EMAIL_PATTERN = 
-	        "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-	        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
 	private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
 
@@ -93,8 +91,7 @@ public class Util {
 	}
 
 	public String getNomeMes(String mes) {
-		String[] meses = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro",
-				"Outubro", "Novembro", "Dezembro" };
+		String[] meses = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
 
 		return meses[Integer.valueOf(mes) - 1];
 	}
@@ -136,7 +133,7 @@ public class Util {
 		Double valorAux = Double.valueOf(0);
 
 		for (BigDecimal valor : lista) {
-			if (valor != null) { 
+			if (valor != null) {
 				valorAux += valor.doubleValue();
 			}
 		}
@@ -211,17 +208,18 @@ public class Util {
 		telefone = telefone.replaceAll("-", "");
 		telefone = telefone.replaceAll("\\(", "");
 		telefone = telefone.replaceAll("\\)", "");
-		if (telefone.length()>=11) {
-			telefone = telefone.substring(0,10);
+		if (telefone.length() >= 11) {
+			telefone = telefone.substring(0, 10);
 		}
 		return telefone.trim();
 	}
-	
+
 	public String trataEmail(String email) {
 		if (email != null) {
 			email = email.trim();
 			if (email != null && email.trim().isEmpty()) {
-				email = null;return email;
+				email = null;
+				return email;
 			}
 			if (!email.contains("@")) {
 				email = null;
@@ -237,13 +235,13 @@ public class Util {
 					if (email.contains(";br")) {
 						email = email.replace(";br", ".br");
 					} else if (email.contains(";")) {
-						 int posicaoPv = email.indexOf(";");
-						 email = email.substring(0, posicaoPv);
+						int posicaoPv = email.indexOf(";");
+						email = email.substring(0, posicaoPv);
 					}
 				} else {
 					email = null;
 				}
-				
+
 			}
 		}
 		return email;
@@ -255,17 +253,17 @@ public class Util {
 		}
 		return conteudo;
 	}
-	
+
 	public boolean isEmptyOrNull(String content) {
-		
+
 		if (content == null) {
 			return true;
-		} 
-		
+		}
+
 		if (content.trim().isEmpty()) {
 			return true;
 		}
-		
+
 		return false;
 	}
 
@@ -286,15 +284,15 @@ public class Util {
 		}
 		return conteudo;
 	}
-	
+
 	public Date getStringToDateHoursMinutesAdd2hours(String data) {
 		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		try {
 			Date date = (Date) formatter.parse(data);
 			GregorianCalendar gc = new GregorianCalendar();
-		    gc.setTime(date);
-		    gc.add(Calendar.HOUR,2);
-		    return gc.getTime();
+			gc.setTime(date);
+			gc.add(Calendar.HOUR, 2);
+			return gc.getTime();
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
@@ -302,131 +300,157 @@ public class Util {
 		return null;
 	}
 
-	
-	public static boolean validarEmail(String email){
-	    Matcher matcher = pattern.matcher(email);
-	    return matcher.matches();
-	 }
+	public static boolean validarEmail(String email) {
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
 
-	private static final int[] pesoCPF = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
-	   private static final int[] pesoCNPJ = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+	public static boolean validarCpf(String cpf) {
+		if ((cpf == null) || (cpf.length() != 11))
+			return false;
+		// considera-se erro cpf's formados por uma sequencia de numeros iguais
+		if (cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222") || cpf.equals("33333333333") || cpf.equals("44444444444")
+				|| cpf.equals("55555555555") || cpf.equals("66666666666") || cpf.equals("77777777777") || cpf.equals("88888888888") || cpf.equals("99999999999")
+				|| (cpf.length() != 11))
+			return (false);
 
-	   public static boolean validarCpf(String cpf) {
-	      if ((cpf==null) || (cpf.length()!=11)) return false;
-	   // considera-se erro cpf's formados por uma sequencia de numeros iguais
-	      if (cpf.equals("00000000000") || cpf.equals("11111111111") ||
-	          cpf.equals("22222222222") || cpf.equals("33333333333") ||
-	          cpf.equals("44444444444") || cpf.equals("55555555555") ||
-	          cpf.equals("66666666666") || cpf.equals("77777777777") ||
-	          cpf.equals("88888888888") || cpf.equals("99999999999") ||
-	         (cpf.length() != 11))
-	         return(false);
+		char dig10, dig11;
+		int sm, i, r, num, peso;
 
-	      char dig10, dig11;
-	      int sm, i, r, num, peso;
+		// "try" - protege o codigo para eventuais erros de conversao de tipo
+		// (int)
+		try {
+			// Calculo do 1o. Digito Verificador
+			sm = 0;
+			peso = 10;
+			for (i = 0; i < 9; i++) {
+				// converte o i-esimo caractere do cpf em um numero:
+				// por exemplo, transforma o caractere '0' no inteiro 0
+				// (48 eh a posicao de '0' na tabela ASCII)
+				num = (int) (cpf.charAt(i) - 48);
+				sm = sm + (num * peso);
+				peso = peso - 1;
+			}
 
-	  // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
-	      try {
-	  // Calculo do 1o. Digito Verificador
-	        sm = 0;
-	        peso = 10;
-	        for (i=0; i<9; i++) {              
-	  // converte o i-esimo caractere do cpf em um numero:
-	  // por exemplo, transforma o caractere '0' no inteiro 0         
-	  // (48 eh a posicao de '0' na tabela ASCII)         
-	          num = (int)(cpf.charAt(i) - 48); 
-	          sm = sm + (num * peso);
-	          peso = peso - 1;
-	        }
+			r = 11 - (sm % 11);
+			if ((r == 10) || (r == 11))
+				dig10 = '0';
+			else
+				dig10 = (char) (r + 48); // converte no respectivo caractere
+											// numerico
 
-	        r = 11 - (sm % 11);
-	        if ((r == 10) || (r == 11))
-	           dig10 = '0';
-	        else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+			// Calculo do 2o. Digito Verificador
+			sm = 0;
+			peso = 11;
+			for (i = 0; i < 10; i++) {
+				num = (int) (cpf.charAt(i) - 48);
+				sm = sm + (num * peso);
+				peso = peso - 1;
+			}
 
-	  // Calculo do 2o. Digito Verificador
-	        sm = 0;
-	        peso = 11;
-	        for(i=0; i<10; i++) {
-	          num = (int)(cpf.charAt(i) - 48);
-	          sm = sm + (num * peso);
-	          peso = peso - 1;
-	        }
+			r = 11 - (sm % 11);
+			if ((r == 10) || (r == 11))
+				dig11 = '0';
+			else
+				dig11 = (char) (r + 48);
 
-	        r = 11 - (sm % 11);
-	        if ((r == 10) || (r == 11))
-	           dig11 = '0';
-	        else dig11 = (char)(r + 48);
+			// Verifica se os digitos calculados conferem com os digitos
+			// informados.
+			if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10)))
+				return (true);
+			else
+				return (false);
+		} catch (InputMismatchException erro) {
+			return (false);
+		}
 
-	  // Verifica se os digitos calculados conferem com os digitos informados.
-	        if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10)))
-	           return(true);
-	        else return(false);
-	      } catch (InputMismatchException erro) {
-	          return(false);
-	      }
+	}
 
-	   }
+	public static boolean validarCnpj(String cnpj) {
+		if ((cnpj == null) || (cnpj.length() != 14))
+			return false;
+		int soma = 0, aux, dig;
+		String cnpj_calc = cnpj.substring(0, 12);
 
-	   public static boolean validarCnpj(String cnpj) {
-	      if ((cnpj==null)||(cnpj.length()!=14)) return false;
+		if (cnpj.length() != 14)
+			return false;
 
-	      return false;
-	   }
+		char[] chr_cnpj = cnpj.toCharArray();
+
+		/* Primeira parte */
+		for (int i = 0; i < 4; i++)
+			if (chr_cnpj[i] - 48 >= 0 && chr_cnpj[i] - 48 <= 9)
+				soma += (chr_cnpj[i] - 48) * (6 - (i + 1));
+		for (int i = 0; i < 8; i++)
+			if (chr_cnpj[i + 4] - 48 >= 0 && chr_cnpj[i + 4] - 48 <= 9)
+				soma += (chr_cnpj[i + 4] - 48) * (10 - (i + 1));
+		dig = 11 - (soma % 11);
+
+		cnpj_calc += (dig == 10 || dig == 11) ? "0" : Integer.toString(dig);
+
+		/* Segunda parte */
+		soma = 0;
+		for (int i = 0; i < 5; i++)
+			if (chr_cnpj[i] - 48 >= 0 && chr_cnpj[i] - 48 <= 9)
+				soma += (chr_cnpj[i] - 48) * (7 - (i + 1));
+		for (int i = 0; i < 8; i++)
+			if (chr_cnpj[i + 5] - 48 >= 0 && chr_cnpj[i + 5] - 48 <= 9)
+				soma += (chr_cnpj[i + 5] - 48) * (10 - (i + 1));
+		dig = 11 - (soma % 11);
+		cnpj_calc += (dig == 10 || dig == 11) ? "0" : Integer.toString(dig);
+
+		return cnpj.equals(cnpj_calc);
+	}
 
 	public String trataTelefone(String telefone) {
-		if (telefone!=null){
-			if (telefone.length()<10){
-				if (telefone.length()<7){
+		if (telefone != null) {
+			if (telefone.length() < 10) {
+				if (telefone.length() < 7) {
 					return null;
-				}
-				else{
+				} else {
 					return completarZerosEsquerda(telefone, 0);
 				}
-			}
-			else if(telefone.length()>11){
+			} else if (telefone.length() > 11) {
 				return telefone.substring(0, 11);
-			}
-			else{
+			} else {
 				return telefone;
 			}
-		}
-		else{
+		} else {
 			return null;
 		}
 	}
-	
-	public static void main(String args[]){
-		System.out.println(new Util().corrigeDouble(0+12+""));
+
+	public static void main(String args[]) {
+		System.out.println(new Util().corrigeDouble(0 + 12 + ""));
 	}
-	
-	public double corrigeDouble(Object obj){
+
+	public double corrigeDouble(Object obj) {
 		double valor = 0;
-		try{
-			if (obj!=null){
+		try {
+			if (obj != null) {
 				valor = Double.valueOf((double) obj);
 			}
+		} catch (Exception e) {
 		}
-		catch(Exception e){}
-		try{
-			if (obj!=null){
+		try {
+			if (obj != null) {
 				valor = (double) obj;
 			}
+		} catch (Exception e) {
 		}
-		catch(Exception e){}
-		try{
-			if (obj!=null){
+		try {
+			if (obj != null) {
 				valor = Double.valueOf((String) obj);
 			}
+		} catch (Exception e) {
 		}
-		catch(Exception e){}
-		try{
-			if (obj!=null){
+		try {
+			if (obj != null) {
 				valor = Double.valueOf((Integer) obj);
 			}
+		} catch (Exception e) {
 		}
-		catch(Exception e){}
-		
+
 		return valor;
 	}
 }
